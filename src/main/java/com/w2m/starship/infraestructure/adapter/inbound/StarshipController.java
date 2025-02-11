82b9245e6fbaa9d1d157b303cec3aef5039d8650
horadev <horacio@megawebs.com.ar>
2025-02-11 15:11:21 -0300

    feature/search by name

--- src/main/java/com/w2m/starship/infraestructure/adapter/inbound/StarshipController.java
+++ src/main/java/com/w2m/starship/infraestructure/adapter/inbound/StarshipController.java
@@ -6,14 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/starships")
@@ -31,6 +24,12 @@ public class StarshipController {
     public ResponseEntity<Starship> getStarshipById(@PathVariable Long id) {
         return ResponseEntity.ok(starShipService.getStarshipById(id));
     }
+
+    @GetMapping("/search/name")
+    public ResponseEntity<Page<Starship>> getStarshipByName(@RequestParam String name, Pageable pageable) {
+        return ResponseEntity.ok(starShipService.getStarshipByName(name, pageable));
+    }
+
 
     @PostMapping
     public ResponseEntity<Starship> createStarship(@RequestBody Starship starship) {
--- src/main/java/com/w2m/starship/infraestructure/adapter/outbound/StarshipRepository.java
+++ src/main/java/com/w2m/starship/infraestructure/adapter/outbound/StarshipRepository.java
@@ -1,9 +1,12 @@
 package com.w2m.starship.infraestructure.adapter.outbound;
 
 import com.w2m.starship.domain.model.Starship;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StarshipRepository extends JpaRepository<Starship, Long> {
+    Page<Starship> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
