fd7324ffe7a1b00606797ebf3d71bb3948626e6f
horadev <horacio@megawebs.com.ar>
2025-02-11 15:58:32 -0300

    feature/openapi documentation

--- src/main/java/com/w2m/starship/domain/model/Starship.java
+++ src/main/java/com/w2m/starship/domain/model/Starship.java
@@ -1,16 +1,22 @@
 package com.w2m.starship.domain.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
 
+@Schema(name = "Starship", description = "Starship object saving the id and name of a starship")
 @Entity
 @Table(name = "starship")
 @Data
 public class Starship {
+
+    @Schema(description = "The id of the starship")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Schema(description = "The name of the starship")
     @Column(nullable = false, unique = true)
+    @NonNull
     private String name;
 }
--- src/main/java/com/w2m/starship/infraestructure/adapter/inbound/StarshipController.java
+++ src/main/java/com/w2m/starship/infraestructure/adapter/inbound/StarshipController.java
@@ -2,12 +2,15 @@ package com.w2m.starship.infraestructure.adapter.inbound;
 
 import com.w2m.starship.application.service.StarshipServiceImpl;
 import com.w2m.starship.domain.model.Starship;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Starships API", description = "CRUD operations related to starships")
 @RestController
 @RequestMapping("/starships")
 @RequiredArgsConstructor
@@ -15,32 +18,38 @@ public class StarshipController {
 
     private final StarshipServiceImpl starShipService;
 
+    @Operation(summary = "Get all starships available")
     @GetMapping
     public ResponseEntity<Page<Starship>> getAllStarships(Pageable pageable) {
         return ResponseEntity.ok(starShipService.getAllStarships(pageable));
     }
 
+    @Operation(summary = "Get a starship searching by its id")
     @GetMapping("{id}")
     public ResponseEntity<Starship> getStarshipById(@PathVariable Long id) {
         return ResponseEntity.ok(starShipService.getStarshipById(id));
     }
 
+    @Operation(summary = "Get all starships containg a text on their name ")
     @GetMapping("/search/name")
-    public ResponseEntity<Page<Starship>> getStarshipByName(@RequestParam String name, Pageable pageable) {
-        return ResponseEntity.ok(starShipService.getStarshipByName(name, pageable));
+    public ResponseEntity<Page<Starship>> getStarshipByName(@RequestParam String text, Pageable pageable) {
+        return ResponseEntity.ok(starShipService.getStarshipByName(text, pageable));
     }
 
 
+    @Operation(summary = "Create a starship and save it on db")
     @PostMapping
     public ResponseEntity<Starship> createStarship(@RequestBody Starship starship) {
         return ResponseEntity.ok(starShipService.createStarship(starship));
     }
 
+    @Operation(summary = "Update a starship searching by its id")
     @PutMapping("{id}")
     public ResponseEntity<Starship> updateStarship(@PathVariable Long id, @RequestBody Starship spacecraft) {
         return ResponseEntity.ok(starShipService.updateStarship(id, spacecraft));
     }
 
+    @Operation(summary = "Delete a starship searching by its id")
     @DeleteMapping("{id}")
     public ResponseEntity<Void> deleteStarship(@PathVariable Long id) {
         starShipService.deleteStarship(id);
