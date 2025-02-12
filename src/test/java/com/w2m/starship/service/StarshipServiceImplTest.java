4e26a4fba591cc48698a72e9b6f32acb372eddfe
horadev <horacio@megawebs.com.ar>
2025-02-11 23:49:49 -0300

    fix/test using Redis

--- src/test/java/com/w2m/starship/service/StarshipServiceImplTest.java
+++ src/test/java/com/w2m/starship/service/StarshipServiceImplTest.java
@@ -18,6 +18,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.HttpClientErrorException;
 import java.util.List;
 import java.util.Optional;
@@ -27,6 +28,7 @@ import org.springframework.http.HttpStatus;
 
 //@ExtendWith(MockitoExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class StarshipServiceImplTest {
 
     @MockBean
