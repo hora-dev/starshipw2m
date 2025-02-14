d025b066d3729e816517842e29e7a4b656f402c9
horadev <horacio@megawebs.com.ar>
2025-02-14 12:29:32 -0300

    fix/update security to non deprecated methods

--- src/main/java/com/w2m/starship/infraestructure/config/SpringSecurityConfig.java
+++ src/main/java/com/w2m/starship/infraestructure/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,11 +16,10 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 
-        http.csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( authorize -> authorize
+                        .anyRequest()
+                        .authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
 
